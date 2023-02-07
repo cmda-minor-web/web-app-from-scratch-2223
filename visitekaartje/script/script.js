8d22e67c0ed622b382d76fc2be7c171e4f3914c1
Keïsha Alexander <keisha.alexander@hva.nl>
2023-02-07 14:40:07 +0100

    Visitekaart versie 2

--- visitekaartje/script/script.js
+++ visitekaartje/script/script.js
@@ -1,29 +1,34 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-const request = new XMLHttpRequest()
+// // Create a request variable and assign a new XMLHttpRequest object to it.
+// const request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/members?first=200', true)
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/members?first=200', true)
 
-request.onload = function () {
-  // Begin accessing JSON data here
-  const data = JSON.parse(this.response)
-  const header = document.querySelector("h1");
+// request.onload = function () {
+//   // Begin accessing JSON data here
+//   const data = JSON.parse(this.response)
+//   const header = document.querySelector("h1");
 
  
-  if (request.status >= 200 && request.status < 400) {
-    console.log(data)
-    header.textContent = data.members[89].name + ' ' +data.members[89].surname
+//   if (request.status >= 200 && request.status < 400) {
+//     console.log(data)
+//     header.textContent = data.members[89].name + ' ' +data.members[89].surname
      
   
-  } else {
-    console.log('error')
-  }
+//   } else {
+//     console.log('error')
+//   }
   
-
-}
-
 // Send request
-request.send()
+//request.send()
+// }
+
+//variabele
+const secondHeading = $("section:nth-of-type(1) h2");
+
+console.log(mainHeading)
+
+
 
 
 (function() {
