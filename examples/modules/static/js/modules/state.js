8a1b3799e3736818a0a467df5e116a440693d868
K3A101 <keisha.alexander@hva.nl>
2023-02-13 11:09:25 +0100

    REpo eigen gemaakt

--- examples/modules/static/js/modules/state.js
+++
@@ -1,8 +0,0 @@
-import { $ } from './ui.js'
-
-export function state(current) {
-  const loader = $('div.loader')
-  current === 'loading'
-    ? loader.classList.add('loading')
-    : loader.classList.remove('loading')
-}
