de9b166704a92b454786ddda8f51898a7a9541e4
K3A101 <keisha.alexander@hva.nl>
2023-02-14 15:14:17 +0100

    Data ingeladen

--- spa/script/script.js
+++ spa/script/script.js
@@ -1,7 +1,37 @@
 console.log("Hello");
 
-fetch('https://cors-anywhere.herokuapp.com/https://quote.api.fdnd.nl/v1/quotes')
+
+
+    fetch('https://opensheet.elk.sh/14joQ9h8M0ydoJJ-fNYN68ls3TWPCvk8ZvBJvUXpF1cQ/sheet1')
 .then((response) => response.json())
 .then((data) => {
-    console.log(data)
-})
+  const main = document.querySelector("main ")
+  const img = document.querySelector("main section img")
+console.log(data)
+
+let html = ''
+
+data.forEach(item => {
+
+    html = `
+      <section>
+        <img src="${item.avatar}" alt="Avatar">
+
+        <article>
+            <q>${item.quote}</q>
+
+            <p>${item.author}</p>
+            <p>${item.bio}</p>
+        </article>
+    </section>
+    
+    `;
+
+    main.insertAdjacentHTML('beforeend', html)
+//     const allName = item.author ;
+//    main.insertAdjacentHTML('beforeend', `${allName}`)
+
+});
+    
+});
+
