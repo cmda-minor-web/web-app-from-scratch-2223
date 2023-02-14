81ad8abca2226dba0e5a35bda7935bf3dc7e2e0a
K3A101 <keisha.alexander@hva.nl>
2023-02-14 10:09:46 +0100

    nieuwe url

--- spa/script/script.js
+++ spa/script/script.js
@@ -1,6 +1,6 @@
 console.log("Hello");
 
-fetch('https://quote.api.fdnd.nl/v1/quotes')
+fetch('https://cors-anywhere.herokuapp.com/https://quote.api.fdnd.nl/v1/quotes')
 .then((response) => response.json())
 .then((data) => {
     console.log(data)
