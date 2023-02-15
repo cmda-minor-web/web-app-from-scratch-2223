c295e3bdbe64bee66a48f06e71a8bd291c5532c8
Jevona Magdalena <jmagdalena@ictual.com>
2023-02-15 09:29:21 +0100

    design almost ready and fech data

--- spa/index.js
+++ spa/index.js
@@ -1,13 +1,48 @@
-const article = document.querySelector("section:nth-of-type(1) article p");
-const url = "https://quotes.fdnd.nl/api/v1/quotes"
+const title = document.querySelector("h3");
+const bio = document.querySelector("p")
+const quote = document.querySelector("q")
+const url = "https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes"
+const button = document.querySelector("button")
+let info;
+let counter = 0;
 
-promiseOfSomeData();
 
+window.onload = async () => {
+    promiseOfSomeData();
+    console.log("onload");
+};
+button.addEventListener("click", next)
 
 function promiseOfSomeData() {
-    fetch(url).then((r) => r.json()).then(data => {
-        console.log(data.data[2].text)
-        article.innerHTML = data.data[2].text
+    fetch(url)
+    .then((r) => {
+        if (r.status >= 200 && r.status <= 299){
+            return r.json();
+        }else {
+            throw Error(r.statusText);}
+    })
+    .then((json) => {
+        info = json;
+        console.log(info)
+        console.log(info.length)
+        title.innerHTML = info[counter].author;
+        bio.innerHTML = info[counter].bio;
+        quote.innerHTML = info[counter].quote;
+    })
+    .catch((error) => {
+        console.log(error)
     });
-    
+}
+
+function next(){
+    console.log("binnen")
+    if (counter == info.length){
+        counter = 0
+    } else {
+        counter++
+        console.log(counter)
+        title.innerHTML = info[counter].author;
+        bio.innerHTML = info[counter].bio;
+        quote.innerHTML = info[counter].quote;
+    }
 }
