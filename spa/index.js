5d02b6614b975e371b34fd7a67be293033a71b87
Jevona Magdalena <jmagdalena@ictual.com>
2023-02-16 22:02:28 +0100

    error state added

--- spa/index.js
+++ spa/index.js
@@ -1,48 +1,61 @@
 const title = document.querySelector("h3");
-const bio = document.querySelector("p")
-const quote = document.querySelector("q")
-const url = "https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes"
-const button = document.querySelector("button")
+const bio = document.querySelector("p");
+const quote = document.querySelector("q");
+const id = document.querySelector("span > span")
+const random = document.querySelector("button:nth-of-type(1)");
+const next = document.querySelector("button:nth-of-type(2)");
+const url = "https://opensheet.elk.sh/1p7Wnace8KpaIFnATpBcil_KyJ4P8IC8vYIUO8NCfcKc/Quotes";
 let info;
 let counter = 0;
-
 
 window.onload = async () => {
     promiseOfSomeData();
     console.log("onload");
 };
-button.addEventListener("click", next)
+
+random.addEventListener("click", randomSelector);
+next.addEventListener("click", nextSelector);
 
 function promiseOfSomeData() {
     fetch(url)
-    .then((r) => {
-        if (r.status >= 200 && r.status <= 299){
-            return r.json();
-        }else {
-            throw Error(r.statusText);}
-    })
-    .then((json) => {
-        info = json;
-        console.log(info)
-        console.log(info.length)
-        title.innerHTML = info[counter].author;
-        bio.innerHTML = info[counter].bio;
-        quote.innerHTML = info[counter].quote;
-    })
-    .catch((error) => {
-        console.log(error)
-    });
+        .then((r) => {
+            if (r.status >= 200 && r.status <= 299) {
+                return r.json();
+            } else {
+                throw Error(r.statusText);
+            }
+        })
+        .then((json) => {
+            info = json;
+            console.log(info)
+            console.log(info.length)
+            title.innerHTML = info[counter].author;
+            bio.innerHTML = info[counter].bio;
+            quote.innerHTML = info[counter].quote;
+            id.innerHTML = info[counter].id;
+        })
+        .catch((error) => {
+            alert(error);
+        });
 }
 
-function next(){
-    console.log("binnen")
-    if (counter == info.length){
-        counter = 0
+function nextSelector() {
+    if (counter == info.length) {
+        counter = 0;
     } else {
-        counter++
-        console.log(counter)
+        counter++;
         title.innerHTML = info[counter].author;
         bio.innerHTML = info[counter].bio;
         quote.innerHTML = info[counter].quote;
+        id.innerHTML = info[counter].id;
     }
+}
+
+function randomSelector() {
+    let counterRandom = Math.floor(Math.random() * info.length);
+
+    title.innerHTML = info[counterRandom].author;
+    bio.innerHTML = info[counterRandom].bio;
+    quote.innerHTML = info[counterRandom].quote;
+    id.innerHTML = info[counterRandom].id;
 }
