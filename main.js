c4d0828248031d12a0e9762f5b2f8417c1433ccf
Rachid <rachid.el.ouali@hva.nl>
2023-03-10 09:13:43 +0100

    error state aangepast
    fix

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // app.js
 
-// Import the displayQuote function from the display.js module
+// Import the displayQuote function from the render.js module
 import { displayQuote } from './modules/render.js';
 
 // Add an event listener to the category select element that calls the displayQuote function when the value changes
 const categorySelect = document.getElementById("category-select");
-categorySelect.addEventListener("change", displayQuote);
+categorySelect.addEventListener("change", displayQuote); // call function when value changes
 
 // Call the displayQuote function on DOMContentLoaded and when the "New Quote" button is clicked
 document.addEventListener('DOMContentLoaded', () => {
--- modules/api.js
+++ modules/api.js
@@ -1,7 +1,6 @@
 // Fetches data from the API based on the maximum number of characters in a quote and a specified category
 export async function fetchData(maxChars, category) {
   const url = `https://api.api-ninjas.com/v1/quotes?category=${category}&limit=10`;
-  
 
   try {
     const response = await fetch(url, {
@@ -11,15 +10,12 @@ export async function fetchData(maxChars, category) {
     const data = await response.json(); // Parse the response data as JSON
     const filteredData = data.filter(quote => quote.quote.length <= maxChars); // Filter the data to only include quotes with less than or equal to the specified maximum number of characters
 
+    if (filteredData.length === 0) { // if quote is equal to 0
+      throw new Error('No quotes found'); // throw error
+    }
+
     return filteredData; // Return the filtered data
   } catch (error) {
     console.error('Error fetching data:', error); // Log any errors that occur during the fetch
   }
-
-  const filteredData = data.filter(quote => quote.quote.length <= maxChars); // var used for error handling
-
-  if (filteredData.length === 0) { // if quote is equal to 0
-    throw new Error('No quotes found'); // throw error
-  }
-
 }
